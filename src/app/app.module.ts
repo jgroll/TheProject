8f6b1b9bd5f0287de49ba185ca981e331e08f76d
Julien GROLL <jgroll@ihu-strasbourg.eu>
2018-07-27 14:54:34 +0200

    Trying setting up router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material.module';
+import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '../../node_modules/@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'recette', component: RecipePageComponent },
+  { path: '**', component: HomeComponent},
+];
 
 
+// Import les components générés 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RecipePageComponent } from './recipe/recipe-page/recipe-page.component';
 import { RecipeCardComponent } from './recipe/recipe-card/recipe-card.component';
 import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
-import { ReactiveFormsModule, FormsModule } from '../../node_modules/@angular/forms';
+
 
 
 @NgModule({
@@ -27,7 +37,9 @@ import { ReactiveFormsModule, FormsModule } from '../../node_modules/@angular/fo
     BrowserAnimationsModule,
     MaterialModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes, { useHash: true}),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
